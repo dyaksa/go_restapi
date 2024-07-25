21c4282530c99f3512d9f1a9714575231f840297
dyaksa <diasnour0395@gmail.com>
2024-07-25 08:48:19 +0700

    minor

--- model/entity/category.go
+++
@@ -1,6 +0,0 @@
-package entity
-
-type Category struct {
-	ID   int
-	Name string
-}
--- model/entity/profile.go
+++ model/entity/profile.go
@@ -5,38 +5,6 @@ import (
 	"github.com/google/uuid"
 )
 
-type ProfileV2 struct {
-	ID    uuid.UUID       `db:"id"`
-	Nik   types.AESCipher `db:"nik"`
-	Name  types.AESCipher `db:"name" bidx_col:"name_bidx" txt_heap_table:"profile_text_heap"`
-	Phone types.AESCipher `db:"phone"`
-	Email types.AESCipher `db:"email" bidx_col:"email_bidx" txt_heap_table:"email_text_heap"`
-	DOB   types.AESCipher `db:"dob"`
-}
-
-type FetchProfileRowV2 struct {
-	ID    uuid.UUID
-	Nik   types.AESCipher
-	Name  types.AESCipher
-	Phone types.AESCipher
-	Email types.AESCipher
-	DOB   types.AESCipher
-}
-
-type FetchInvoiceRow struct {
-	ID                uuid.UUID
-	CustomerName      string
-	CustomerNameCrypt types.AESCipher
-	CustomerAddress   string
-	CustomerAddrCrypt types.AESCipher
-}
-
-type FetchInvoiceRowsCrypt struct {
-	IDField      uuid.UUID
-	CustomerName types.AESCipher
-	CustomerAddr types.AESCipher
-}
-
 type Profile struct {
 	ID        uuid.UUID       `db:"id"`
 	Nik       types.AESCipher `db:"nik"`
@@ -48,48 +16,4 @@ type Profile struct {
 	Email     types.AESCipher `db:"email"`
 	EmailBidx string          `db:"email_bidx" txt_heap_table:"email_text_heap"`
 	DOB       types.AESCipher `db:"dob"`
-}
-
-type FetchProfileRow struct {
-	ID    uuid.UUID
-	Nik   types.AESCipher
-	Name  types.AESCipher
-	Phone types.AESCipher
-	Email types.AESCipher
-	DOB   types.AESCipher
-}
-
-type FetchProfileParams struct {
-	ID uuid.UUID
-}
-
-// type FindProfileByIDParams struct {
-// 	ID []uuid.UUID
-// }
-
-// type FindProfileByNameParams struct {
-// 	NameBidx types.BIDXString
-// }
-
-// type FindProfileByEmailParams struct {
-// 	EmailBidx types.BIDXString
-// }
-
-type FindProfilesByNameRow struct {
-	ID    uuid.UUID
-	Nik   types.AESCipher
-	Name  types.AESCipher
-	Phone types.AESCipher
-	Email types.AESCipher
-	Dob   types.AESCipher
-}
-
-type FindProfileByParams struct {
-	Type    string
-	Content string
-}
-
-type FindProfileByBIDXParams struct {
-	ColumnHeap string
-	Hash       []string
 }
