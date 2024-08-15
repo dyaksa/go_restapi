dd38aee83a48a3c08c4fe91310e70de77fb53ae7
dyaksa <diasnour0395@gmail.com>
2024-08-15 12:52:17 +0700

    chore: enhance env

--- app/app.go
+++ app/app.go
@@ -10,10 +10,10 @@ import (
 func SetupRouter(profileHandler handler.ProfileHandler) *httprouter.Router {
 	route := httprouter.New()
 
-	route.POST("/perf-test/go", profileHandler.Create)
-	route.GET("/perf-test/go", profileHandler.FindAll)
-	route.GET("/perf-test/go/:id", profileHandler.FetchProfile)
-	route.PUT("/perf-test/go/:id", profileHandler.Update)
+	route.POST("/perf-test-go", profileHandler.Create)
+	route.GET("/perf-test-go", profileHandler.FindAll)
+	route.GET("/perf-test-go/:id", profileHandler.FetchProfile)
+	route.PUT("/perf-test-go/:id", profileHandler.Update)
 
 	route.PanicHandler = exception.ErrorHandler
 	return route
